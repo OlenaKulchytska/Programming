9fe28ad33f4e6adb22893f78609f3b892446504f
KappasTV <43045902+KappasTV@users.noreply.github.com>
2018-11-18 14:00:59 +0200

    Add files via upload

--- Lab_6/main.c
+++ Lab_6/main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main() {
+int main()
+{
     char text[255];
     int num=0;
-
-/*    printf("Enter string: \n");
-    scanf("%s",text);
-    printf("%s",text);*/
-
     printf("Enter string: ");
     gets(text);
     char *token = strtok(text," ");
@@ -19,25 +16,15 @@ int main() {
         for (int i = 0; i <strlen(token) ;i++) {
 
 
-            if (token[i] == '1'||token[i] == '2'||token[i] == '3'||token[i] == '4'||token[i] == '5'||token[i] == '6'||token[i] == '7'||token[i] == '8'||token[i] == '9'||token[i] == '0'){
+            if (isdigit(token[i])){
                 num+=1;
-
-
-
            }
-
-
         }
         if(num==1)
         {
             printf("%s\n", token);
         }
-
         num=0;
-
-
-
-
         token = strtok(NULL, " ");
     }
 
